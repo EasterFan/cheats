d51f2947e7b8222a983bf5645911127b5287cc6d
easterfan <easterfan@163.com>
2022-09-21 09:35:06 +0800

    Auot-update

--- cron/git_auto_update.sh
+++ cron/git_auto_update.sh
@@ -5,3 +5,6 @@ cd /Users/${USER}/Library/Application\ Support/navi/cheats/EasterFan__navi-cheat
 git add . && git commit -m 'Auot-update' && git pull origin master --rebase && git push origin master 
 # 运行完成
 echo 'finish' >> /Users/${USER}/Workspace/easter_log/log
+
+
+
